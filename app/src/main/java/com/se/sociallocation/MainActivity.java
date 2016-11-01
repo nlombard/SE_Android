742ccf1f465b5564179db3f2ba926d4c345cf3a4
kthienem <kthienem@nd.edu>
2016-11-01 12:48:18 -0400

    Commented out extraneous file

--- app/src/main/java/com/se/sociallocation/MainActivity.java
+++ app/src/main/java/com/se/sociallocation/MainActivity.java
@@ -26,7 +26,7 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 //import com.google.android.gms.drive.Permission;
 //import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.CameraUpdateFactory;
+//import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 //import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
